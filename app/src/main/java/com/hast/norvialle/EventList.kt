4978c367357360ee1f2fb3f3303cc29c0cc23ec8
Evolution <evostudios@driversed.com>
2019-09-20 02:23:27 +0300

    Premared main screen

---
+++ app/src/main/java/com/hast/norvialle/EventList.kt
@@ -0,0 +1,8 @@
+package com.hast.norvialle
+
+/**
+ * Created by Konstantyn Zakharchenko on 19.09.2019.
+ */
+class EventList(val date: String, val events: ArrayList<Event>)
+
+class Event(val name: String, val time: String)
--- app/src/main/java/com/hast/norvialle/MainActivity.kt
+++ app/src/main/java/com/hast/norvialle/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.hast.norvialle
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    
+    val presenter: MainPresenter = MainPresenter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        list.layoutManager = LinearLayoutManager(this)
+        list.adapter = EventsAdapter(presenter.events, this)
+
     }
+
 }
